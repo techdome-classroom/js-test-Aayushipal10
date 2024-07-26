205fde59d11a37bc7e5c5c75ef02ba8efd155f21
Aayushipal10 <141208659+Aayushipal10@users.noreply.github.com>
2024-07-26 14:24:32 +0530

    Add files via upload

--- program1.js
+++ program1.js
@@ -1,8 +1,32 @@
 const getTotalIsles = function (grid) {
 
+  if (!grid || grid.length === 0) return 0;
 
-  // write your code here
+  let count = 0;
+  const dfs = (grid, i, j) => {
+    
+    if (i < 0 || j < 0 || i >= grid.length || j >= grid[i].length || grid[i][j] === 'W') return;
 
+
+    grid[i][j] = 'W';
+
+
+    dfs(grid, i + 1, j); // down
+    dfs(grid, i - 1, j); // up
+    dfs(grid, i, j + 1); // right
+    dfs(grid, i, j - 1); // left
+  };
+
+  for (let i = 0; i < grid.length; i++) {
+    for (let j = 0; j < grid[i].length; j++) {
+      if (grid[i][j] === 'L') {
+        count++;
+        dfs(grid, i, j);
+      }
+    }
+  }
+
+  return count;
 };
 
 module.exports = getTotalIsles;
